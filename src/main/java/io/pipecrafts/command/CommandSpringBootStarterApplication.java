287754a9f74dc8b6d299ef512b0f3cbba041d789
Aaron Rasing <maaronrasing@gmail.com>
2025-06-30 23:24:50 +0800

    Initial commit

---
+++ src/main/java/io/pipecrafts/command/CommandSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.pipecrafts.command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandSpringBootStarterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommandSpringBootStarterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/pipecrafts/command/CommandSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pipecrafts.command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandSpringBootStarterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
